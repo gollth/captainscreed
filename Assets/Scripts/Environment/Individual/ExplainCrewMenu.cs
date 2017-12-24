ab7f729adffdebfc0a4471ea7e055badd8458037
Thore Goll <thoregoll@googlemail.com>
2017-12-24 12:20:09 +0100

    add explaination of crew menu script add indivudal folder, holding scripts only applicable to very specific situations, such as explanations in certain levels

---
+++ Assets/Scripts/Environment/Individual/ExplainCrewMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplainCrewMenu : MonoBehaviour {
+
+	[SerializeField] string thrustersExplanationKey = "";
+	[SerializeField] string reloadersExplanationKey = "";
+	[SerializeField] string repairersExplanationKey = "";
+	
+
+	void OnEnable () {
+		var explainedThrusters = false;
+		var explainedReloaders = false;
+		var explainedRepairers = false;
+
+		ResourceLoader.WaitFor <Crew> (tag: "Player", callback: crew => {
+			var majority = Mathf.RoundToInt (crew.amount * .6f);
+			crew.OnOrder.AddListener(() => {
+
+				// Explain what reloaders do
+				if (crew.Reloaders > majority && !explainedReloaders) {
+					Bubble.MessageByFirstOfficer(reloadersExplanationKey);
+					explainedReloaders = true;
+				}
+
+				// Explain what repairers do
+				if (crew.Repairers > majority && !explainedRepairers) {
+					Bubble.MessageByFirstOfficer(repairersExplanationKey);
+					explainedRepairers = true;
+				}
+
+				// Expalin what thrusters do
+				if (crew.Thrusters > majority && !explainedThrusters) {
+					Bubble.MessageByFirstOfficer(thrustersExplanationKey);
+					explainedThrusters = true;
+				}
+			});
+		});		
+	}
+
+}
